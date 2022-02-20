66a93bddfe065e9af710702f42a94d9148d8f88a
kochenov <kochenof@gmail.com>
2022-02-20 10:05:31 +0300

    Внёс не значительные изменения, добавил коменты

--- src/store/cart.js
+++ src/store/cart.js
@@ -14,19 +14,23 @@ export default {
     getCartStatus: (state) => state.cartStatus,
   },
   mutations: {
-    // Установка корзины, выгрузка товара в корзину
+    // Установка корзины, выгрузка товара в STATE корзины
     setCart: (state, cart) => {
       state.cart = cart;
     },
-    // Добавление товара в корзину
+    /**
+     * Добавление товара в STATE корзины
+     * @param {*} state
+     * @param {*} product
+     */
     setAddToCart: (state, product) => {
-      let productCart = state.cart.find((item) => item.id === product.id);
-      if (typeof productCart === "undefined") {
+      if (!state.cart.some((item) => item.id === product.id)) {
         state.cart.push(product);
         ++state.countProductsInCart;
+        state.sumPriceProductInCart += product.price;
       }
     },
-    // Добавление товара в корзину
+    // Изменение значения товара в STATE корзины,
     setEditCart: (state, product) => {
       let indx = state.cart.findIndex((item) => item.id === product.id);
       state.cart[indx] = product;
@@ -39,6 +43,11 @@ export default {
     setCountProductsInCartMinus: (state) => {
       --state.countProductsInCart;
     },
+    /**
+     * Перерасчёт количества товаров при манипуляции товарами в корзине
+     * @param {*} state
+     * @param {*} products
+     */
     setCountProductsInCart: (state, products) => {
       let count = 0;
       products.forEach((product) => {
@@ -58,7 +67,10 @@ export default {
     setCartStatusSwith: (state) => {
       state.cartStatus = !state.cartStatus;
     },
-    // Общая сумма в корзине
+    /**
+     * Общая стоимость товаров корзины / перерасчёт при манипуляции товарами в корзине
+     * @param {*} state
+     */
     setSumPriceProductInCart: (state) => {
       let price = 0;
       state.cart.forEach((product) => {
@@ -66,16 +78,28 @@ export default {
       });
       state.sumPriceProductInCart = price;
     },
+    /**
+     * Удаляет из STATE корзины конкретный товар
+     * Исключает значения товара (шт и цену) из общего стека
+     * @param {*} state
+     * @param {*} product
+     */
     setCartDeliteProduct: (state, product) => {
       let indx = state.cart.findIndex((p) => p.id == product.id);
       state.countProductsInCart -= product.count;
+      state.sumPriceProductInCart -= product.count * product.price;
       state.cart.splice(indx, 1);
     },
   },
   actions: {
+    /**
+     * Показывает или скрывает окно корзины с товарами
+     * @param {Object} commit
+     */
     actionSwithCatrStatus({ commit }) {
       commit("setCartStatusSwith");
     },
+    // Загружает из БД в STATE данные корзины
     loadProductsFromCart({ commit }) {
       axios({
         method: "GET",
@@ -98,6 +122,11 @@ export default {
         })
         .finally(() => {});
     },
+    /**
+     * Изменяет товар в корзине, а именно его количество, и итоговую цену
+     * @param {*} commit
+     * @param {*} product
+     */
     actionEditProductFromCart({ commit }, product) {
       ++product.count;
       axios({
@@ -112,10 +141,8 @@ export default {
         },
       })
         .then(() => {
-          //commit("setAddToCart", product);
           commit("setCountProductsInCartPlus");
           commit("setSumPriceProductInCart");
-          //console.log(response.data);
         })
         .catch((error) => {
           console.log(error);
@@ -123,12 +150,16 @@ export default {
         .finally(() => {});
       commit("setCartStatusOpen");
     },
+    /**
+     * Добавляет товар в корзину и записывает данные в базу
+     * Перед добавлением проверяет есть ли товар в корзине (если есть то не добавляет),
+     * сделано для того, что бы при множественном клике на кнопку добавить в корзину товар не дублировался
+     * на кнопку добавления товара
+     * @param {*} commit
+     * @param {*} product
+     */
     actionAddProductToCart({ commit }, product) {
-      let productCart = this.getters.getCart.find(
-        (item) => item.id === product.id
-      );
-      console.log(productCart);
-      if (typeof productCart === "undefined") {
+      if (!this.getters.getCart.some((item) => item.id === product.id)) {
         // Если в корзине есть товар
         product.count = 1;
         axios({
@@ -144,33 +175,32 @@ export default {
         })
           .then(() => {
             commit("setAddToCart", product);
-            commit("setSumPriceProductInCart");
-            //console.log(response.data);
           })
           .catch((error) => {
             console.log(error);
           })
           .finally(() => {});
 
-        commit("setCartStatusOpen");
+        commit("setCartStatusOpen"); // Открывает окно корзины
       }
     },
+    /**
+     * Экшен удаления товаров из корзины. Удаляет сразу любое количество товара данного артикукла
+     * Производит запись данных в базу через API
+     * @param {*} commit
+     * @param {*} product
+     */
     actionDeleteProductFromCart({ commit }, product) {
       axios({
         method: "DELETE",
         url: `/api/v1/cart/delete/${product.id}`,
-        params: {
-          //user_key_id: "USER_KEY_ID",
-        },
+        params: {},
         headers: {
           "Content-type": "application/json; charset=UTF-8",
         },
       })
         .then(() => {
           commit("setCartDeliteProduct", product);
-          commit("setSumPriceProductInCart");
-
-          //console.log(response.data);
         })
         .catch((error) => {
           console.log(error);
